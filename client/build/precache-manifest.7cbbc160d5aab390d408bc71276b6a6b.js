ac3dcd3a5eaebae8666576134b37227a8c7627b9
AndrianCode <anpojda@gmail.com>
2018-11-09 14:41:41 +0200

    front production

---
+++ client/build/precache-manifest.7cbbc160d5aab390d408bc71276b6a6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f824c71a4a04e52f514c",
+    "url": "/static/js/main.f824c71a.chunk.js"
+  },
+  {
+    "revision": "5b863acfc4b02f21c3d8",
+    "url": "/static/js/1.5b863acf.chunk.js"
+  },
+  {
+    "revision": "f824c71a4a04e52f514c",
+    "url": "/static/css/main.a57961ce.chunk.css"
+  },
+  {
+    "revision": "c478fa8f4601961f9b760ee4e7cb4654",
+    "url": "/index.html"
+  }
+];
